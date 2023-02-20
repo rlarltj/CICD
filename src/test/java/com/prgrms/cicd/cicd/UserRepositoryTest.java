a1caace9cf797286a482276118a794e583f24ce4
김기서 <5rne@naver.com>
2023-02-21 02:46:05 +0900

    commit from develop

--- src/test/java/com/prgrms/cicd/cicd/UserRepositoryTest.java
+++ src/test/java/com/prgrms/cicd/cicd/UserRepositoryTest.java
@@ -25,4 +25,18 @@ class UserRepositoryTest {
 
 		Assertions.assertEquals(findUser, user);
 	}
+
+
+	@Test
+	void test5() {
+		User user = User.builder()
+			.username("testUser5")
+			.build();
+
+		userRepository.save(user);
+
+		User findUser = userRepository.findById(user.getId()).get();
+
+		Assertions.assertEquals(findUser, user);
+	}
 }
